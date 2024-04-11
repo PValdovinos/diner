d2a956261ae46251a1b62dafd4241554a3590697
Pedro V.R. <ptreyes2@gmail.com>
2024-04-11 12:46:34 -0400

    added things

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+// 328/diner/index.php
+// This is my CONTROLLER
+
+// Turn on error reporting
+require_once ('vendor/autoload.php');
+
+// Instantiate base class
+$f3-> Base::instance();
+
+//define a default route
+$f3->route('GET /', function(){
+    //echo '<h1>Welcome to my diner app!</h1>';
+
+    //render a view page
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+// Run Fat-Free
+$f3->run();
+
+?>

d18ffb0c2d820366b42a21281f9983842c4c6504
Pedro V.R. <ptreyes2@gmail.com>
2024-04-18 12:09:50 -0400

    Added pages

--- index.php
+++ index.php
@@ -1,37 +1,89 @@
 <?php
 
-
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
 // 328/diner/index.php
-// This is my CONTROLLER
+// This is my CONTROLLER!
 
 // Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
 require_once ('vendor/autoload.php');
 
-// Instantiate base class
+// Instantiate the F3 Base class
 $f3 = Base::instance();
 
-//define a default route
-$f3->route('GET /', function(){
-    //echo '<h1>Welcome to my diner app!</h1>';
+// Define a default route
+// https://tostrander.greenriverdev.com/328/hello-fat-free/
+$f3->route('GET /', function() {
+    //echo '<h1>Hello from My Diner App!</h1>';
 
-    //render a view page
+    // Render a view page
     $view = new Template();
-    echo $view->render('views/home.html');
+    echo $view->render('views/home-page.html');
 });
 
-$f3->route('GET /menus/breakfast', function(){
-    echo '<h1>My Breakfast Menu</h1>';
+// Breakfast menu
+$f3->route('GET /menus/breakfast', function() {
+    //echo '<h1>My Breakfast Menu</h1>';
 
-    /*
-     * Render a view page
+    // Render a view page
     $view = new Template();
-    echo $view->render('views/home.html');
-    */
+    echo $view->render('views/breakfast-menu.html');
+});
+
+// Lunch menu
+$f3->route('GET /menus/lunch', function() {
+    //echo '<h1>My Breakfast Menu</h1>';
+
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/lunch-menu.html');
+});
+
+// Dinner menu
+$f3->route('GET /menus/dinner', function() {
+    //echo '<h1>My Breakfast Menu</h1>';
+
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/dinner-menu.html');
+});
+
+// Order Form Part I
+$f3->route('GET|POST /order1', function($f3) {
+    //echo '<h1>My Breakfast Menu</h1>';
+
+    // If the form has been posted
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+        //echo "<p>You got here using the POST method</p>";
+        //var_dump ($_POST);
+
+        // Get the data from the post array
+        $food = $_POST['food'];
+        $meal = $_POST['meal'];
+
+        // If the data valid
+        if (true) {
+            $f3->set('SESSION.food', $food);
+            $f3->set('SESSION.meal', $meal);
+        }
+    }
+
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/order1.html');
+});
+
+// Order Form Part II
+$f3->route('GET /order2', function() {
+    //echo '<h1>My Breakfast Menu</h1>';
+
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/order2.html');
 });
 
 // Run Fat-Free
 $f3->run();
-
-?>
